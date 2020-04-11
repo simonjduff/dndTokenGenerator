fbf36fc6a1b88ec1739d0ffd433be53786a065fc
Simon Duff <simon.j.duff@gmail.com>
2020-04-11 10:16:09 +0100

    Make parameters more extensible

--- src/TokenGenerator/Parameters.cs
+++ src/TokenGenerator/Parameters.cs
@@ -1,21 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace TokenGenerator
 {
     public class Parameters
     {
+        private enum States
+        {
+            TokenText,
+            BackgroundColour
+        }
+
         public Parameters(params string[] args)
         {
-            int colourIndex = Array.IndexOf(args, "-c");
-            if (colourIndex > -1)
+            States state = States.TokenText;
+            List<string> tokenText = new List<string>();
+
+            foreach (string arg in args)
             {
-                Colour = new Colour(args[colourIndex + 1]);
-                Range textRange = Range.StartAt(colourIndex + 2);
-                TokenText = string.Join("\n", args[textRange]);
-                return;
+                if (arg == "-c")
+                {
+                    state = States.BackgroundColour;
+                    continue;
+                }
+
+                switch (state)
+                {
+                    case States.BackgroundColour:
+                        Colour = new Colour(arg);
+                        state = States.TokenText;
+                        continue;
+                    case States.TokenText:
+                        tokenText.Add(arg);
+                        continue;
+                }
             }
 
-            TokenText = string.Join("\n", args);
+            TokenText = string.Join(" ", tokenText);
         }
 
         public Colour Colour { get; } = new Colour("#FFFFFF");
